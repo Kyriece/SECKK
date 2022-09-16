0e57884fbc37ed70ec54c3cfd55e8ec34acb50e0
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-16 21:07:01 +1000

    commit test

--- php/Register.php
+++ php/Register.php
@@ -6,21 +6,12 @@ include('Data_Access.php');
     <body>
         <?php
         //Receive user input from clint side
-        $user_name = $_POST['username'];
+        $user_input = $_POST['username'];
         $user_password = $_POST['userPassword'];
 
-        echo $user_name;
+        echo $user_input;
         echo $user_password;
-/*
-        $returnPass = "";
-        $db = openDB();
-        $ret = getCustomerPassword($db, $user_name);
-        while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-            $returnPass = $row['userName'];
-        }
-
-        echo $returnPass;
-        closeDB($db);*/
+        
         ?>
     </body>
 </html>
