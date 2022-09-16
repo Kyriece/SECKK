a900d7de0715f9d24804cbc589f9b27c144ff1cb
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-16 21:28:14 +1000

    commit test

--- php/Login.php
+++ php/Login.php
@@ -1,9 +1,14 @@
+<?php
+include('Data_Access.php');
+?>
+
 <html>
     <body>
         <?php
-        
         $user_input = $_POST['username'];
+        $user_password = $_POST['userPassword'];
         echo $user_input;
+        echo $user_password;
         ?>
     </body>
 </html>
