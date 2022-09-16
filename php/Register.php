c5465a5b2d0b700c2fc2dcc9f46837fc4dfacb9b
Kai Run Leong <s3862092@csitprdap01.int.its.rmit.edu.au>
2022-09-16 17:14:27 +1000

    test

--- php/Register.php
+++ php/Register.php
@@ -7,7 +7,7 @@ include('Data_Access.php');
         <?php
         //Receive user input from clint side
         $user_input = $_POST['username'];
-        $user_password = $POST['userPassword'];
+        $user_password = $_POST['userPassword'];
 
         echo $user_input;
         echo $user_password;
