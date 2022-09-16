0ca8e54466814a75b3e1c2d1af80813981035755
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-16 21:39:56 +1000

    commit test

--- php/Login.php
+++ php/Login.php
@@ -13,6 +13,15 @@ include('Data_Access.php');
         while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
             $userPass = $row['userPassword'];
         }
+
+        if($user_password == $userPass){
+            header("Location: ../template/home.html", TRUE, 301);
+            exit();
+        }else{
+            header("Location: ../template/LoginFail.html", TRUE, 301);
+            exit();
+        }
+
         echo "The password is: ".$userPass;
         ?>
     </body>
