e9862cffd6df076627d94a885fc215ef25c7cf70
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-19 19:41:39 +1000

    fixed bug

--- php/Data_Access.php
+++ php/Data_Access.php
@@ -70,8 +70,8 @@ function createNewUser($dbReturned, $user_name, $user_password, $user_email,
     //can use query to update
     //see this https://www.w3schools.com/php/php_mysql_insert.asp
     $sql = <<<EOF
-        INSERT INTO customer (userName, userEmail, userFirstName, userLastName, userPassword, userPhoneNumber)
-        VALUES("$username", "$user_password", "$user_email", "$user_first", "$user_last", "$user_phone");
+        INSERT INTO Customer (userName, userEmail, userFirstName, userLastName, userPassword, userPhoneNumber)
+        VALUES("$user_name", "$user_password", "$user_email", "$user_first", "$user_last", "$user_phone");
     EOF;
 
     $db->query($sql);
