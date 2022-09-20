b9518085561b35cc771a6d2a713717f3e6e8881e
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-20 23:06:21 +1000

    test

--- php/Login.php
+++ php/Login.php
@@ -18,7 +18,7 @@ include('Data_Access.php');
         while($row = $ret2->fetchArray(SQLITE3_ASSOC) ) {
             $user_role = $row['userRole'];
         }
-        
+        /*
         if($user_password == $userPass && $userRole == 'user'){
             header("Location: ../template/home.html", TRUE, 301);
             exit();
@@ -31,6 +31,8 @@ include('Data_Access.php');
             header("Location: ../template/LoginFail.html", TRUE, 301);
             exit();
         }
+        */
+        echo $user_role;
         closeDB($db);
         ?>
     </body>
