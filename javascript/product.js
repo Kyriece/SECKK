f941650678fa2df692d19a1b759b96deaf7fcb1e
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-21 16:02:15 +1000

    test

--- javascript/product.js
+++ javascript/product.js
@@ -43,6 +43,38 @@ class UI{
         let result = '';
         products.forEach(product => {
             result += `
+            <div class = "product-grid-container">
+            <!--Product 1-->
+            <div class = "card-wrapper">
+                <div class = "card">
+                  <!-- card left -->
+                  <div class = "product-imgs">
+                    <img src="../images/copper.png" alt="">
+                  </div>
+                  <!-- card right -->
+                  <div class = "product-content">
+                    <h2 class = "product-title">Copper Moon</h2>
+                    <div class = "product-price">
+                      <p>Price: <span>$30</span></p>
+                    </div>
+          
+                    <div class = "product-detail">
+                      <h2>about this item: </h2>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet veniam tempora fuga tenetur placeat sapiente architecto illum soluta consequuntur, aspernatur quidem at sequi ipsa!</p>
+                    </div>
+          
+                    <div class = "purchase-info">
+                      <input type = "number" min = "0" value = "1">
+                      <button type = "button" class = "btn">
+                        Add to Cart <i class = "fas fa-shopping-cart"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+            /*
+            result += `
             <!-- single product -->
             <article class = "product">
                 <div class = "img-container">
@@ -57,7 +89,7 @@ class UI{
             </article>
             <!--end single product -->
             
-            `;
+            `;*/
         });
         productsDOM.innerHTML = result; 
     }
