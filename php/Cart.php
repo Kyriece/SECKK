218097d2cf04beff3cb5326f7d348b25c86ee68d
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-22 15:26:15 +1000

    test

--- php/Cart.php
+++ php/Cart.php
@@ -28,7 +28,7 @@ include('Data_Access.php');
                 for ($x = 0; $x < $index; $x++) {
                     echo $products[$x] . " " . $quantity[$x] . "\n";
                 }
-                $db = openDB();
+                closeDB($db);
 
                 updateCartQuantity($curr_user, $product_id, $quantity[$index]++);
                 
