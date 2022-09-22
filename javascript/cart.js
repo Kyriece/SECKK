3370d6074367857a0a145566ed214ab830ecb3da
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-22 23:14:59 +1000

    test

--- javascript/cart.js
+++ javascript/cart.js
@@ -40,7 +40,31 @@ class UI{
         products.forEach(product => {
             //${product.CartID}</br></br>${product.Quantity}
             result += `
-                
+            <div class="cart">
+            <ul class="cartWrap">
+                <li class="items odd">
+                    <div class="infoWrap"> 
+                        <div class="cartSection">
+                            <img src="http://lorempixel.com/output/technics-q-c-300-300-4.jpg" alt="" class="itemImg" />
+                            <p class="itemNumber">#QUE-007544-002</p>
+                            <h3>Item Name 1</h3>
+                          
+                             <p> <input type="text"  class="qty" placeholder="3"/> x $5.00</p>
+                          
+                            <p class="stockStatus"> In Stock</p>
+                        </div>  
+                      
+                          
+                        <div class="prodTotal cartSection">
+                            <p>$15.00</p>
+                         </div>
+                        <div class="cartSection removeWrap">
+                            <a href="#" class="remove">x</a>
+                        </div>  
+                    </div>
+                </li>
+            </ul>
+        </div>
             `;
         });
         productsDOM.innerHTML = result; 
