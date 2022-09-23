e1240e18dea6481d3d3dd52502488292f1d97fb4
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-23 15:48:23 +1000

    Test

--- php/Logout.php
+++ php/Logout.php
@@ -4,7 +4,7 @@ include('Data_Access.php');
 <html>
     <body>
         <?php
-            if (!file_exists('currentUser.txt')) {
+            if (file_exists('currentUser.txt')) {
                 $myfile = fopen("currentUser.txt", "r") or die("Unable to open file!");
                 $curr_user = fread($myfile,filesize("currentUser.txt"));
                 fclose($myfile);
@@ -12,12 +12,12 @@ include('Data_Access.php');
             }
 
             //If cart.json exists
-            if (!file_exists('cart.json')) {
+            if (file_exists('cart.json')) {
                 unlink('cart.json');
             }
 
             //If currentUser.txt exist
-            if (!file_exists('currentUser.txt')) {
+            if (file_exists('currentUser.txt')) {
                 unlink('currentUser.txt');
             }
             
