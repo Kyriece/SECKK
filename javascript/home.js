de8e8881bab54c62a42722a4dd991dbed0183923
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-24 23:13:51 +1000

    test

--- javascript/home.js
+++ javascript/home.js
@@ -15,10 +15,16 @@ class UI{
 
 document.addEventListener("DOMContentLoaded", ()=>{
     // Open a log file
-    var file = new File("../php/currentUser.txt");
-    console.log(file.exists());
+    //var file = new File("../php/currentUser.txt");
+    //console.log(file.exists());
     //if(file.exists()){
      //   const ui = new UI();
      //   ui.displayProducts();
    // }
+
+   if(system.functions.isfileexist("../php/currentUser.txt")){
+        const ui = new UI();
+        ui.displayProducts();
+   }
+
 });
