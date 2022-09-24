1d342d9f683a64cd205fce0d1409c4e18d0aa22b
Kai Run Leong <S3862092@student.rmit.edu.au>
2022-09-24 22:40:11 +1000

    test

---
+++ javascript/home.js
@@ -0,0 +1,23 @@
+const productsDOM = document.querySelector('.addCart');
+
+// display products, Responsible for getting all of the items that are being returned by the product
+class UI{
+    displayProducts(){
+        let result = '';
+  
+        result += `
+            <div class = "cart-center"></div>
+            `;
+        productsDOM.innerHTML = result; 
+    }
+    
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    // Open a log file
+    var file = new File("../php/Cart.php");
+    if(file.exists()){
+        const ui = new UI();
+        ui.displayProducts(products);
+    }
+});
